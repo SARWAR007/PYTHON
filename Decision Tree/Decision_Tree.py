f2fde365822c51098369f7897505d80ddd665133
farhan700 <faisalfarhan700@gmail.com>
2020-01-31 23:45:23 +0530

    Decision Tree been loaded

---
+++ Decision Tree/Decision_Tree.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 21:20:21 2020
+
+@author: faisa
+"""
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+
+dataset =pd.read_csv(r"weather.csv")
+
+dataset
